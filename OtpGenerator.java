d36aea5120944753f35d5344301a806296d5c19b
sumanth4567 <119740442+sumanth4567@users.noreply.github.com>
2023-05-01 15:19:25 +0530

    Update OtpGenerator.java

--- OtpGenerator.java
+++ OtpGenerator.java
@@ -3,17 +3,17 @@ public class OtpGenerator {
     public static void main(String[] args)
     {
         int length = 6;
-        System.out.println(geek_Password(length));
+        System.out.println(password(length));
     }
-    static char[] geek_Password(int len)
+    static char[] password(int len)
     {
         System.out.println("Generating password using random() : ");
         System.out.print("Your new password is : ");
         String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String Small_chars = "abcdefghijklmnopqrstuvwxyz";
         String numbers = "0123456789";
-        //String symbols = "!@#$%^&*_=+-/.?<>)";
-        String values = Capital_chars + Small_chars +numbers; //+ symbols;
+        String symbols = "!@#$%^&*_=+-/.?<>)";
+        String values = Capital_chars + Small_chars +numbers+ symbols;
         Random rndm_method = new Random();
         char[] password = new char[len];
         for (int i = 0; i < len; i++)
